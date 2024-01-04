63948ebc94a37c13bb0c0114294652acc08d5f60
Cleiton-D <cleitonahnerth@gmail.com>
2024-01-04 08:18:36 -0400

    teste

--- lib/new-client/app/routes/$.tsx
+++ lib/new-client/app/routes/$.tsx
@@ -5,10 +5,12 @@ import type {LoaderFunctionArgs} from "@remix-run/node"
 export const loader = ({request}:LoaderFunctionArgs) => {
     const url = new URL(request.url)
 
+    
     const host = process.env.LEGACY_URL || url.origin
     const pathname = `/legacy${url.pathname}`
-
+    
     url.href = `${host}${pathname}`
+    console.log("caiu aqui", url.toString())
 
     return redirectDocument(url.toString())
 }
