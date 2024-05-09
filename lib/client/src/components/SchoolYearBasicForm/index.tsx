621a02e91184ee6e2a9185fe47e61994b47314da
Cleiton Dione Ahnerth Kiper <32417778+Cleiton-D@users.noreply.github.com>
2024-05-09 07:48:36 -0400

    Update index.tsx

--- lib/client/src/components/SchoolYearBasicForm/index.tsx
+++ lib/client/src/components/SchoolYearBasicForm/index.tsx
@@ -42,7 +42,8 @@ const referenceYearOptions = [
   { label: '2020', value: '2020' },
   { label: '2021', value: '2021' },
   { label: '2022', value: '2022' },
-  { label: '2023', value: '2023' }
+  { label: '2023', value: '2023' },
+  { label: '2024', value: '2024' }
 ];
 
 const SchoolYearBasicForm: React.ForwardRefRenderFunction<FormHandles> = (
