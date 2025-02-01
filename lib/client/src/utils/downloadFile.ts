afafc389aa3c6672dc75c000c05f531c4dda8d5c
Cleiton Kiper <cleitonahnerth@gmail.com>
2025-02-01 18:31:22 -0400

    teste

--- lib/client/src/utils/downloadFile.ts
+++ lib/client/src/utils/downloadFile.ts
@@ -1,7 +1,6 @@
 export const downloadFile = (object: Blob, filename: string) => {
   if (typeof document === 'undefined') return;
 
-
   const url = window.URL.createObjectURL(object);
   const link = document.createElement('a');
   link.href = url;
