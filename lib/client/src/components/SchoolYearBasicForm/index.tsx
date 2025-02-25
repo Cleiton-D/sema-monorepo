b58689c6f098274654c64aed624f9dd2fa42bf16
Daniel Carlos Mação <67325305+danielcarlosmacao@users.noreply.github.com>
2025-02-25 14:36:43 -0400

    Update index.tsx
    adição de ano referencia 2025 e 2026.

--- lib/client/src/components/SchoolYearBasicForm/index.tsx
+++ lib/client/src/components/SchoolYearBasicForm/index.tsx
@@ -43,7 +43,9 @@ const referenceYearOptions = [
   { label: '2021', value: '2021' },
   { label: '2022', value: '2022' },
   { label: '2023', value: '2023' },
-  { label: '2024', value: '2024' }
+  { label: '2024', value: '2024' },
+  { label: '2025', value: '2025' },
+  { label: '2026', value: '2026' }
 ];
 
 const SchoolYearBasicForm: React.ForwardRefRenderFunction<FormHandles> = (
