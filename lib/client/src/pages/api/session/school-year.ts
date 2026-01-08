d9e0700ebfec2b13fb382ec9eca7669419537223
Cleiton Kiper <cleitonahnerth@gmail.com>
2026-01-08 13:47:33 -0400

    test: test client

--- lib/client/src/pages/api/session/school-year.ts
+++ lib/client/src/pages/api/session/school-year.ts
@@ -11,17 +11,19 @@ const getSchoolYearRoute: NextApiHandler = async (req, res) => {
     return res.status(403).send({ error: 'unauthorized' });
   }
 
-  const { scy } = decode(req.session.token) as {
+  console.log('Session token:', req.session.token);
+  const decoded = decode(req.session.token) as {
     pfl: string;
     sub: string;
     scy?: string;
   };
 
-  console.log('Decoded token:', { scy });
+  console.log('Decoded token:', decoded);
+  console.log('scy:', decoded.scy);
 
   const api = createUnstableApi(req.session);
   const schoolYear = await api
-    .get<AccessModule[]>(`/education/admin/school-years/${scy || 'current'}`)
+    .get<AccessModule[]>(`/education/admin/school-years/${decoded.scy || 'current'}`)
     .then((response) => response.data);
 
   res.send(schoolYear);
