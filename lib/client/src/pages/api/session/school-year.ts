818fa671d137e9b6754ff26c0b6a5347a67aeeb2
Cleiton Kiper <cleitonahnerth@gmail.com>
2026-01-08 15:27:08 -0400

    test: test client

--- lib/client/src/pages/api/session/school-year.ts
+++ lib/client/src/pages/api/session/school-year.ts
@@ -1,34 +1,37 @@
 import { NextApiHandler } from 'next';
-import { decode } from 'jsonwebtoken';
+// import { decode } from 'jsonwebtoken';
 
-import { AccessModule } from 'models/AccessModule';
+// import { AccessModule } from 'models/AccessModule';
 
-import { createUnstableApi } from 'services/api';
-import { withSessionRoute } from 'utils/session/withSession';
+// import { createUnstableApi } from 'services/api';
+// import { withSessionRoute } from 'utils/session/withSession';
 
 const getSchoolYearRoute: NextApiHandler = async (req, res) => {
   console.log('getSchoolYearRoute called');
   console.log('request data', req);
-  if (!req.session.token) {
-    return res.status(403).send({ error: 'unauthorized' });
-  }
 
-  console.log('Session token:', req.session.token);
-  const decoded = decode(req.session.token) as {
-    pfl: string;
-    sub: string;
-    scy?: string;
-  };
+  res.status(200).json({ message: 'Funciona sem imports' });
 
-  console.log('Decoded token:', decoded);
-  console.log('scy:', decoded.scy);
+  // if (!req.session.token) {
+  //   return res.status(403).send({ error: 'unauthorized' });
+  // }
 
-  const api = createUnstableApi(req.session);
-  const schoolYear = await api
-    .get<AccessModule[]>(`/education/admin/school-years/${decoded.scy || 'current'}`)
-    .then((response) => response.data);
+  // console.log('Session token:', req.session.token);
+  // const decoded = decode(req.session.token) as {
+  //   pfl: string;
+  //   sub: string;
+  //   scy?: string;
+  // };
 
-  res.send(schoolYear);
+  // console.log('Decoded token:', decoded);
+  // console.log('scy:', decoded.scy);
+
+  // const api = createUnstableApi(req.session);
+  // const schoolYear = await api
+  //   .get<AccessModule[]>(`/education/admin/school-years/${decoded.scy || 'current'}`)
+  //   .then((response) => response.data);
+
+  // res.send(schoolYear);
 };
 
 export default getSchoolYearRoute;
