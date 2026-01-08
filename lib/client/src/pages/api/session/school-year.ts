dcc7fcccbea03deb001d56a52bef1ff9b8e6cad5
Cleiton Kiper <cleitonahnerth@gmail.com>
2026-01-08 17:14:57 -0400

    test: test client

--- lib/client/src/pages/api/session/school-year.ts
+++ lib/client/src/pages/api/session/school-year.ts
@@ -7,12 +7,12 @@ import { createUnstableApi } from 'services/api';
 import { withSessionRoute } from 'utils/session/withSession';
 
 const getSchoolYearRoute: NextApiHandler = async (req, res) => {
-  console.log('decode', decode);
-  // console.log('createUnstableApi', createUnstableApi);
-  // console.log('withSessionRoute', withSessionRoute);
+  // console.log('decode', decode);
+  console.log('createUnstableApi', createUnstableApi);
+  console.log('withSessionRoute', withSessionRoute);
 
-  // const teste: AccessModule = {} as AccessModule;
-  // console.log('teste', teste);
+  const teste: AccessModule = {} as AccessModule;
+  console.log('teste', teste);
 
   res.status(200).json({ message: 'Funciona sem imports' });
 
